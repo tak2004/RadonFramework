e1e679f1f94ddc8b493e6ecc34cb1a4e49df4003
TAK2004 <thomas@ptscientists.com>
2014-03-24 23:44:54 +0100

    -added constexpr definition and compiler detection

--- include/RadonFramework/Compiler.hpp
+++ include/RadonFramework/Compiler.hpp
@@ -4,42 +4,58 @@
 #pragma once
 #endif
 
-//namespace RadonFramework
-//{
-    #if defined(RF_VISUALCPP)
-        // vsc++
+#if defined(RF_VISUALCPP)
+    // vsc++
 
-        // error msg
-        #define __STR2__(x) #x
-        #define __STR1__(x) __STR2__(x)
-        #define __LOC__ __FILE__ "("__STR1__(__LINE__)") : Warning Msg: "		
-        #define RF_COMPILER_WARNING(x) __pragma(message(__LOC__ x))
+    // error msg
+    #define __STR2__(x) #x
+    #define __STR1__(x) __STR2__(x)
+    #define __LOC__ __FILE__ "("__STR1__(__LINE__)") : Warning Msg: "		
+    #define RF_COMPILER_WARNING(x) __pragma(message(__LOC__ x))
 
-        // memory alignment
-        #define ALIGN(X) __declspec(align(X))
-    #else
-        #if defined(RF_MINGW32)
-            // mingw
+    // memory alignment
+    #define ALIGN(X) __declspec(align(X))
+#else
+    #if defined(RF_MINGW32)
+        // mingw
             
+        // error msg
+        #define RF_COMPILER_WARNING(x) #warning x
+    #else
+        #if defined(RF_GCC)
+            // gcc
+
             // error msg
-            #define RF_COMPILER_WARNING(x) #warning x
+            // really bad solution but this is allready the best on gcc
+            #define DO_PRAGMA(x) _Pragma (#x)
+            #define RF_COMPILER_WARNING(x) DO_PRAGMA(message ("warning: " x))
         #else
-            #if defined(RF_GCC)
-                // gcc
-
-                // error msg
-                // really bad solution but this is allready the best on gcc
-                #define DO_PRAGMA(x) _Pragma (#x)
-                #define RF_COMPILER_WARNING(x) DO_PRAGMA(message ("warning: " x))
-            #else
-                static_assert(false,"There's no support, of compiler warnings, for your compiler at the moment.\
-                    Please insert the warning command for your compiler here or remove this info.");                
-            #endif
+            static_assert(false,"There's no support, of compiler warnings, for your compiler at the moment.\
+                Please insert the warning command for your compiler here or remove this info.");                
         #endif
-
-        // memory alignment
-        #define ALIGN(X) __attribute__ ((aligned (X)))
     #endif
-//}
+
+    // memory alignment
+    #define ALIGN(X) __attribute__ ((aligned (X)))
+#endif
+
+// detect if compiler can use constexpr
+// gcc: available since 4.0.6
+#if (__GNUC__ * 100 + __GNUC_MINOR__) >= 406
+    #define RF_HAVE_CONSTEXPR
+#endif
+// clang: check if __has_feature is available
+#ifndef __has_feature
+    #define __has_feature(x) 0
+#endif
+#if __has_feature(cxx_constexpr)
+    #define RF_HAVE_CONSTEXPR
+#endif
+
+#ifdef RF_HAVE_CONSTEXPR
+    #define RF_CONSTEXPR constexpr
+#else
+    #define RF_CONSTEXPR
+#endif
 
 #endif // RF_COMPILER_HPP
