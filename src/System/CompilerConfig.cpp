f6a7544e4303b7825ac7226b1f15511563bd4ede
TAK2004 <thomas@ptscientists.com>
2015-06-04 22:52:22 +0200

    -removed auto generated file to avoid merge conflicts

--- src/System/CompilerConfig.cpp
+++
@@ -1,9 +0,0 @@
-#include "RadonFramework/precompiled.hpp"
-#include "RadonFramework/System/CompilerConfig.hpp"
-
-using namespace RadonFramework::System;
-using namespace RadonFramework::Core::Common;
-
-const Endian::Type CompilerConfig::Endianness=Endian::Little;
-const MemoryArchitecture::Type CompilerConfig::CompiledForArchitecture=MemoryArchitecture::_64Bit;
-const Version CompilerConfig::FrameworkVersion=Version(0,3,0,77);
