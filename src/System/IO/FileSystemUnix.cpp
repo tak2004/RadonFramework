d7aba2cf1b4234deabfb80c98b6c469977cada61
Thomas Kunze <tak2004@ptscientists.com>
2015-11-26 17:31:41 +0100

    -fixed a bug in the String constructor which tried to copy on 0 byte string size -added SystemPathToUri function

--- src/System/IO/FileSystemUnix.cpp
+++ src/System/IO/FileSystemUnix.cpp
@@ -4,6 +4,7 @@
 #include "RadonFramework/Collections/List.hpp"
 #include "RadonFramework/Collections/Queue.hpp"
 #include "RadonFramework/IO/Log.hpp"
+#include "RadonFramework/IO/Uri.hpp"
 
 using namespace RadonFramework::Core::Types;
 using namespace RadonFramework::Core::Common;
@@ -23,59 +24,63 @@ using namespace RadonFramework::System::IO::FileSystem;
 #include <unistd.h>
 #include <stdio.h>
 
-inline Bool AccessUnix(const String& Path, const AccessMode::Type Mode)
+namespace RadonFramework { namespace System { namespace IO { namespace FileSystem {
+
+namespace Unix {
+
+inline Bool Access(const String& Path, const AccessMode::Type Mode)
 {
-    int modes[AccessMode::MAX]={-1, F_OK, R_OK, F_OK|R_OK, W_OK, W_OK|F_OK, 
-                                W_OK|R_OK, W_OK|R_OK|F_OK, X_OK, X_OK|F_OK,
-                                X_OK|R_OK, X_OK|R_OK|F_OK, X_OK|W_OK,
-                                X_OK|W_OK|F_OK, X_OK|W_OK|R_OK, X_OK|W_OK|R_OK|F_OK};
-    Assert(modes[Mode]>=0 && modes[Mode]<=X_OK|W_OK|R_OK|F_OK,"Invalid argument.");
-    return access(Path.c_str(),modes[Mode]);
+    int modes[AccessMode::MAX] = {-1, F_OK, R_OK, F_OK | R_OK, W_OK, W_OK | F_OK,
+                                W_OK | R_OK, W_OK | R_OK | F_OK, X_OK, X_OK | F_OK,
+                                X_OK | R_OK, X_OK | R_OK | F_OK, X_OK | W_OK,
+                                X_OK | W_OK | F_OK, X_OK | W_OK | R_OK, X_OK | W_OK | R_OK | F_OK};
+    Assert(modes[Mode] >= 0 && modes[Mode] <= X_OK | W_OK | R_OK | F_OK, "Invalid argument.");
+    return access(Path.c_str(), modes[Mode]);
 }
 
-inline AutoPointer<FileStatus> StatUnix(const String& Path)
+inline AutoPointer<FileStatus> Stat(const String& Path)
 {
     AutoPointer<FileStatus> result;
     struct stat buf;
-    if (stat(Path.c_str(),&buf)==0)
+    if(stat(Path.c_str(), &buf) == 0)
     {
-        result=AutoPointer<FileStatus>(new FileStatus);
-        result->Size=buf.st_size;
-        result->CreateionTimestamp=buf.st_ctime;
-        result->LastAccessTimestamp=buf.st_atime;
-        result->LastModificationTimestamp=buf.st_mtime;
-        result->IsDirectory=(buf.st_mode & S_IFDIR)!=0;
-        result->IsHidden=false;//some linux distributions hide on '.' some on '~' no restrictive rule.
+        result = AutoPointer<FileStatus>(new FileStatus);
+        result->Size = buf.st_size;
+        result->CreateionTimestamp = buf.st_ctime;
+        result->LastAccessTimestamp = buf.st_atime;
+        result->LastModificationTimestamp = buf.st_mtime;
+        result->IsDirectory = (buf.st_mode & S_IFDIR) != 0;
+        result->IsHidden = false;//some linux distributions hide on '.' some on '~' no restrictive rule.
     }
     return result;
 }
 
-void RealPathUnix(const String& Path, String& ResolvedPath)
+void RealPath(const String& Path, String& ResolvedPath)
 {
     char* resolvedPath = realpath(Path.c_str(), 0);
     if(resolvedPath)
     {
         ResolvedPath = String(resolvedPath, strlen(resolvedPath), RF_Common::DataManagment::TransfereOwnership);
-    }    
+    }
 }
 
-Bool ChangeModeUnix( const String& Path, const AccessMode::Type NewMode )
+Bool ChangeMode(const String& Path, const AccessMode::Type NewMode)
 {
-    int modes[AccessMode::MAX]={-1, F_OK, R_OK, F_OK|R_OK, W_OK, W_OK|F_OK, 
-                                W_OK|R_OK, W_OK|R_OK|F_OK, X_OK, X_OK|F_OK,
-                                X_OK|R_OK, X_OK|R_OK|F_OK, X_OK|W_OK,
-                                X_OK|W_OK|F_OK, X_OK|W_OK|R_OK, X_OK|W_OK|R_OK|F_OK};
-    return chmod(Path.c_str(),modes[NewMode])==0;
+    int modes[AccessMode::MAX] = {-1, F_OK, R_OK, F_OK | R_OK, W_OK, W_OK | F_OK,
+                                W_OK | R_OK, W_OK | R_OK | F_OK, X_OK, X_OK | F_OK,
+                                X_OK | R_OK, X_OK | R_OK | F_OK, X_OK | W_OK,
+                                X_OK | W_OK | F_OK, X_OK | W_OK | R_OK, X_OK | W_OK | R_OK | F_OK};
+    return chmod(Path.c_str(), modes[NewMode]) == 0;
 }
 
-Bool CreatePreAllocatedFileUnix(const String& Path, const Size FileSize)
+Bool CreatePreAllocatedFile(const String& Path, const Size FileSize)
 {
     Bool result = false;
     int fd = open(Path.c_str(), O_CREAT);
     if(fd)
     {
 #ifdef RF_HAVE_POSIX_FALLOCATE
-        result=posix_fallocate(fd,0,FileSize)==0;
+        result = posix_fallocate(fd, 0, FileSize) == 0;
 #else
         struct stat buf;
         if(!fstat(fd, &buf))
@@ -101,84 +106,84 @@ Bool CreatePreAllocatedFileUnix(const String& Path, const Size FileSize)
     return result;
 }
 
-String WorkingDirectoryUnix()
+String WorkingDirectory()
 {
     long size = pathconf(".", _PC_PATH_MAX);
     AutoPointerArray<char> buffer(size);
     String result;
-    if (getcwd(buffer.Get(),size))
-        result=String(buffer.Release().Ptr,size, DataManagment::TransfereOwnership);
+    if(getcwd(buffer.Get(), size))
+        result = String(buffer.Release().Ptr, size, DataManagment::TransfereOwnership);
     return result;
 }
 
-String HomeDirectoryUnix()
+String HomeDirectory()
 {
-    return String::UnsafeStringCreation(getenv ("HOME"));
+    return String::UnsafeStringCreation(getenv("HOME"));
 }
 
-String ApplicationDirectoryUnix()
+String ApplicationDirectory()
 {
     char buffer[BUFSIZ];
     readlink("/proc/self/exe", buffer, BUFSIZ);
     return String(buffer);
 }
 
-Bool ChangeDirectoryUnix( const String& Destination ) 
+Bool ChangeDirectory(const String& Destination)
 {
-    return chdir(Destination.c_str())==0;
+    return chdir(Destination.c_str()) == 0;
 }
 
-Bool CreateDirectoryUnix( const String& Path ) 
-{    
-    return mkdir(Path.c_str(),S_IRWXU | S_IRWXG | S_IRWXO)==0;
+Bool CreateDirectory(const String& Path)
+{
+    return mkdir(Path.c_str(), S_IRWXU | S_IRWXG | S_IRWXO) == 0;
 }
 
-AutoPointerArray<String> DirectoryContentUnix(const String& Path) 
+AutoPointerArray<String> DirectoryContent(const String& Path)
 {
     AutoPointerArray<String> result;
     List<String> list;
-    DIR* dir=0;
-    dir=opendir(Path.c_str());
-    if (dir)
+    DIR* dir = 0;
+    dir = opendir(Path.c_str());
+    if(dir)
     {
-        struct dirent* direntry=0;
-        while ((direntry=readdir(dir))!=0)
+        struct dirent* direntry = 0;
+        while((direntry = readdir(dir)) != 0)
             list.AddLast(String(direntry->d_name));
         closedir(dir);
-        result=AutoPointerArray<String>(list.Count());
-        for (UInt32 i=0;i<result.Count();++i)
+        result = AutoPointerArray<String>(list.Count());
+        for(UInt32 i = 0; i < result.Count(); ++i)
             result[i].Swap(list[i]);
-    }    
-    return result;
-}
-
-Bool CreateFileUnix(const String& Path)
-{
-    Bool result=false;
-    FILE* f=fopen(Path.c_str(),"w");
-    if (f!=0)
-    {
-        fclose(f);
-        result=true;
     }
     return result;
 }
 
-Bool CopyFileUnix(const String& From, const String& To)
+Bool CreateFile(const String& Path)
 {
-    Bool result=false;
+    Bool result = false;
+    FILE* f = fopen(Path.c_str(), "w");
+    if(f != 0)
+    {
+        fclose(f);
+        result = true;
+    }
+    return result;
+}
+
+Bool CopyFile(const String& From, const String& To)
+{
+    Bool result = false;
     int in_fd = open(From.c_str(), O_RDONLY);
 #ifdef RF_HAVE_POSIX_FADVISE
-    posix_fadvise(in_fd, 0,0,POSIX_FADV_SEQUENTIAL);
+    posix_fadvise(in_fd, 0, 0, POSIX_FADV_SEQUENTIAL);
 #else
-    #ifdef RF_HAVE_FREADAHEAD
+#ifdef RF_HAVE_FREADAHEAD
     fcntl(in_fd, F_READAHEAD, 1);
-    #endif
 #endif
-    if (in_fd)
+#endif
+    if(in_fd)
     {
-        AutoPointer<FileStatus> stats=::Stat(From);
-        if (stats)
+        AutoPointer<FileStatus> stats = ::Stat(From);
+        if(stats)
         {
             CreatePreAllocatedFile(To, stats->Size);
             int out_fd = open(To.c_str(), O_WRONLY);
@@ -219,12 +224,12 @@ int GetNativeAccessPriority(const FileAccessPriority::Type Priority)
 }
 #endif
 
-FileHandle OpenFileUnix(const RF_Type::String& Filepath, const FileAccessMode::Type AccessMode,
-                    const FileAccessPriority::Type AccessPriority)
+FileHandle OpenFile(const RF_Type::String& Filepath, const FileAccessMode::Type AccessMode,
+    const FileAccessPriority::Type AccessPriority)
 {
     FileHandle result = FileHandle::Zero();
     int file = open(Filepath.c_str(), GetNativeFlags(AccessMode));
-    if (file != 0)
+    if(file != 0)
     {
 #ifdef RF_HAVE_POSIX_FADVISE
         // set file access pattern advise
@@ -235,7 +240,7 @@ FileHandle OpenFileUnix(const RF_Type::String& Filepath, const FileAccessMode::T
     return result;
 }
 
-RF_Type::Bool CloseFileUnix(FileHandle& Handle)
+RF_Type::Bool CloseFile(FileHandle& Handle)
 {
     int file = static_cast<int>(Handle.GetID());
     Bool result = close(file) == 0;
@@ -243,86 +248,131 @@ RF_Type::Bool CloseFileUnix(FileHandle& Handle)
     return result;
 }
 
-Bool ReadFileUnix(const FileHandle& Handle, UInt8* Buffer, const UInt64 ReadBytes, UInt64& BytesRead)
+MemoryMappingHandle MapFileIntoMemory(const FileHandle& Handle)
+{
+    MemoryMappingHandle result;
+    int file = static_cast<int>(Handle.GetID());
+    if(file != 0)
+    {
+        void* mapping = mmap(0, 0, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, file, 0);
+        if(mapping)
+        {
+            result = MemoryMappingHandle::GenerateFromPointer(mapping);
+        }
+    }
+    return result;
+}
+
+RF_Type::Bool UnmapMemoryFile(MemoryMappingHandle& Handle)
+{
+    RF_Type::Bool result = false;
+    if(Handle.GetPointer() != 0)
+    {
+        munmap(Handle.GetPointer(), 0);
+    }
+    return result;
+}
+
+void* GetMemoryFile(const MemoryMappingHandle& Handle)
+{
+    return Handle.GetPointer();
+}
+
+Bool ReadFile(const FileHandle& Handle, UInt8* Buffer, const UInt64 ReadBytes, UInt64& BytesRead)
 {
     int file = static_cast<int>(Handle.GetID());
     BytesRead = read(file, Buffer, ReadBytes);
     return ReadBytes == BytesRead;
 }
 
-Bool WriteFileUnix(const FileHandle& Handle, const UInt8* Buffer, const UInt64 WriteBytes, UInt64& BytesWritten)
+Bool WriteFile(const FileHandle& Handle, const UInt8* Buffer, const UInt64 WriteBytes, UInt64& BytesWritten)
 {
     int file = static_cast<int>(Handle.GetID());
     return write(file, Buffer, WriteBytes) > 0;
 }
 
-Bool FlushFileUnix(const FileHandle& Handle)
+Bool FlushFile(const FileHandle& Handle)
 {
     int file = static_cast<int>(Handle.GetID());
     return fsync(file) == 0;
 }
 
-UInt64 SeekFileUnix(const FileHandle& Handle, const Int64 Offset, const SeekOrigin::Type Origin)
+UInt64 SeekFile(const FileHandle& Handle, const Int64 Offset, const SeekOrigin::Type Origin)
 {
-    static const int NativeSeek[SeekOrigin::MAX] = {SEEK_SET, SEEK_CUR, SEEK_END };
+    static const int NativeSeek[SeekOrigin::MAX] = {SEEK_SET, SEEK_CUR, SEEK_END};
     int file = static_cast<int>(Handle.GetID());
     return lseek(file, Offset, NativeSeek[Origin]);
 }
 
-UInt64 TellFileUnix(const FileHandle& Handle)
+UInt64 TellFile(const FileHandle& Handle)
 {
     int file = static_cast<int>(Handle.GetID());
     return lseek(file, 0, SEEK_CUR);
 }
 
-RF_Type::String PathSeperatorUnix()
+RF_Type::String PathSeperator()
 {
     static const RF_Type::String result(";");
     return result;
 }
 
-RF_Type::String SeperatorUnix()
+RF_Type::String Seperator()
 {
     static const RF_Type::String result("/");
     return result;
 }
 
-Bool DeleteFileUnix(const String& Path)
+Bool DeleteFile(const String& Path)
 {
     return unlink(Path.c_str()) == 0;
 }
 
-Bool RenameFileUnix(const String& From, const String& To)
+Bool RenameFile(const String& From, const String& To)
 {
     return rename(From.c_str(), To.c_str()) == 0;
 }
 
-void RFFILE::Dispatch()
+RF_Type::Bool SystemPathToUri(const RF_Type::String& SystemPath,
+    RF_IO::Uri& UriInterpretation)
 {
-    OpenFile = OpenFileUnix;
-    CloseFile = CloseFileUnix;
-    ReadFile = ReadFileUnix;
-    WriteFile = WriteFileUnix;
-    FlushFile = FlushFileUnix;
-    SeekFile = SeekFileUnix;
-    TellFile = TellFileUnix;
-    Access = AccessUnix;
-    PathSeperator = PathSeperatorUnix;
-    Seperator = SeperatorUnix;
-    Stat = StatUnix;
-    RealPath = RealPathUnix;
-    ChangeMode = ChangeModeUnix;
-    CreatePreAllocatedFile = CreatePreAllocatedFileUnix;
-    CreateFile = CreateFileUnix;
-    CopyFile = CopyFileUnix;
-    RenameFile = RenameFileUnix;
-    DeleteFile = DeleteFileUnix;
-    WorkingDirectory = WorkingDirectoryUnix;
-    HomeDirectory = HomeDirectoryUnix;
-    ApplicationDirectory = ApplicationDirectoryUnix;
-    ChangeDirectory = ChangeDirectoryUnix;
-    CreateDirectory = CreateDirectoryUnix;
-    DirectoryContent = DirectoryContentUnix;
+    RF_Type::String uriPath = RF_Type::String("file://")+SystemPath;
+    RF_IO::Uri uri1(uriPath, RF_IO::UriKind::Absolute);
+    UriInterpretation = uri1;
+    return true;
+}
+
+}
+
+void Dispatch()
+{
+    OpenFile = Unix::OpenFile;
+    CloseFile = Unix::CloseFile;
+    MapFileIntoMemory = Unix::MapFileIntoMemory;
+    UnmapMemoryFile = Unix::UnmapMemoryFile;
+    GetMemoryFile = Unix::GetMemoryFile;
+    ReadFile = Unix::ReadFile;
+    WriteFile = Unix::WriteFile;
+    FlushFile = Unix::FlushFile;
+    SeekFile = Unix::SeekFile;
+    TellFile = Unix::TellFile;
+    Access = Unix::Access;
+    PathSeperator = Unix::PathSeperator;
+    Seperator = Unix::Seperator;
+    Stat = Unix::Stat;
+    RealPath = Unix::RealPath;
+    ChangeMode = Unix::ChangeMode;
+    CreatePreAllocatedFile = Unix::CreatePreAllocatedFile;
+    CreateFile = Unix::CreateFile;
+    CopyFile = Unix::CopyFile;
+    RenameFile = Unix::RenameFile;
+    DeleteFile = Unix::DeleteFile;
+    WorkingDirectory = Unix::WorkingDirectory;
+    HomeDirectory = Unix::HomeDirectory;
+    ApplicationDirectory = Unix::ApplicationDirectory;
+    ChangeDirectory = Unix::ChangeDirectory;
+    CreateDirectory = Unix::CreateDirectory;
+    DirectoryContent = Unix::DirectoryContent;
+    SystemPathToUri = Unix::SystemPathToUri;
 
 #ifdef RF_LINUX
     extern void Dispatch_Linux();
@@ -334,3 +384,5 @@ void RFFILE::Dispatch()
 #endif
 #endif
 }
+
+} } } }
